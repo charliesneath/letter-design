1eae856dcc5886fb80b1b35594972f072d05a876
charliesneath <charlie.sneath@gmail.com>
2015-10-18 16:36:19 -0400

    Change letter on timer and click
    Letter will automatically change after one minute, or on click.
    
    Click resets timer.

--- scripts/letters.js
+++ scripts/letters.js
@@ -1,66 +1,53 @@
 ---
 ---
 
-$(function() {
-  var numTypefaces = {{ site.static_files.size }};
+var numTypefaces = {{ site.static_files.size }};
+var paths = [];
+var names = [];
+var typefaces = new Array;
+var letters = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
 
-  var paths = [];
+$(function() {
+  init();
+
+  $('#letter').click(function() {
+    nextLetter();
+  });
+})
+
+function init() {
   {% for font in site.static_files %}
     paths.push("{{ font.path }}");
   {% endfor %}
 
-  var names = [];
   for (i=0; i < numTypefaces; i++) {
     var name = paths[i].replace('/fonts/', '');
     name = name.substr(0, name.indexOf('.'));
     names.push(name);
-  }
+  };
 
-  var typefaces = new Array;
   for (i = 0; i < numTypefaces; i++) {
     typefaces[i] = {
         name: names[i],
         path: paths[i]
-    }
-  }
+    };
+  };
+  nextLetter();
+};
 
-  var letters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-  ];
-
+function nextLetter() {
   // Pick a typeface.
   var typefaceId = getRandomInt(0, parseInt(numTypefaces));
   // Pick a letter.
   var letterId = getRandomInt(0, letters.length);
   $('body').css('font-family', names[typefaceId]);
-  $('body').css('font-size', $('.wrapper').height() * .9);
-  $('.letter').html(letters[letterId]);
-})
+  $('body').css('font-size', $('.wrapper').height() * .8);
+  $('#letter').html(letters[letterId]);
+  window.clearTimeout(nextLetter);
+  var switchLetter = window.setTimeout(nextLetter, 60000);
+}
 
 // Get random number.
 function getRandomInt(min, max) {
